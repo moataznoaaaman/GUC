1097a7d1624bda645cf74cd430ad66808b1b9326
moataznoaaaman <63158573+moataznoaaaman@users.noreply.github.com>
2021-01-30 03:18:36 +0200

    release 1.0

--- myapp/src/components/AM/Profile.js
+++ myapp/src/components/AM/Profile.js
@@ -28,15 +28,30 @@ class Profile extends React.Component
 
         .then(res=>
              {  // let reso=res.data
-                 
+                const reso=
+                "Hi "+res.data.name+"\n"+
+                "Day Off: "+res.data.dayoff+"\n"+
+                "Hours per month: "+res.data.hourspermonth+"\n"+
+                "Annual Leave Balance: "+res.data.annualleavebalance+"\n"+
+                "Gender: "+res.data.gender+"\n"+
+                "Missing Days: "+res.data.missingdays+"\n"+
+                "Missing Hours: "+res.data.missinghours+"\n"+
+                "Extra Hours: "+res.data.extrahours+"\n"+
+                "Worked Hours: "+res.data.workedhours+"\n"+
+                "ID: "+res.data.id+"\n"+
+                "Email: "+res.data.email+"\n"+
+                "Salary: "+res.data.salary+"\n"+
+                "Office: "+res.data.office+"\n"+
+                "Department: "+res.data.department
                 this.setState(//{ dayoff: 'monday', hourspermonth: 164.8, annualleavebalance: -6, gender: 'male', notifications: [ 'request :rfrf is accepted ' ], missingdays: 0, missinghours: 0, extrahours: 0, workedhours: 0, name: 'ci1', id: 'am-1', salary: 60, office: 'req.body.office', userType: 'ci', department: 'cs', __v: 0 }
                     {
                         info:<Card style={{ width: '30rem',height:'30rem' }}>
                         <Card.Body>
                         <Card.Title>Profile</Card.Title>
                         <Card.Text>
-                         {"id:"+res.data.id+"  ||name:"+res.data.name+"  ||dayoff:"+res.data.dayoff+"  ||annual leave balance:"+res.data.annualleavebalance+"  ||hours per month:"+res.data.hourspermonth+"  ||missing days:"+res.data.missingdays+"  ||missing hours:"+res.data.missinghours+"  ||extrahours:"+res.data.extrahours+"  ||worked hours:"+res.data.workedhours+"  ||salary:"+res.data.salary+"  ||office:"+res.data.office+"  ||email:"+res.data.email} 
-                        </Card.Text>
+                        {reso.split("\n").map((i,key) => {
+                                return <div key={key}>{i}</div>;
+                            })}</Card.Text>
                         </Card.Body>
                      </Card>
                     }
